c0edad5cafe087d0c34cfc19028727aaa6105151
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2016-11-06 23:35:40 -0700

    Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CS3030Homework6
+
+Jonathan Mirabile
+
+This script processes an Apache server error log file and displays the top 25 errors.
+The user provides the URL as an argument and the script uses urlopen to retrieve the file.
---
+++ jonathan_mirabile_hw6.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 Jonathan <jonathanmirabile@mail.weber.edu>
+#
+# Distributed under terms of the MIT license.
+import sys
+
+"""
+
+"""
+
+# Main function
+def main():
+    return
+
+
+if __name__ == "__main__":
+    # Call Main
+    main()
+
+    exit(0)
+
