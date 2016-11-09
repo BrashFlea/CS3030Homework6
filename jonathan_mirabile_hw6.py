80550f477613c7fc29190c690665cd4046ccc0c8
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2016-11-08 23:21:23 -0700

    Created urlopen shell. Need to handle turning file content into dictionary

--- jonathan_mirabile_hw6.py
+++ jonathan_mirabile_hw6.py
@@ -6,13 +6,39 @@
 #
 # Distributed under terms of the MIT license.
 import sys
-
+import urllib
+import urllib.request
 """
-
+Apache Server Error Log Report Tool
+Args:
+    URL of an Apache server error log file
+Returns:
+    Print report of top 25 errors in log file to screen
 """
+#TODO Remove before submisson
+url = "http://icarus.cs.weber.edu/~hvalle/cs3030/data/error.log.test"
+
+def fetch_url(url):
+    """
+    Fetch list of errors from URL
+    Args:
+        url: The URL of an Apache error log file
+    Returns:
+        A list of errors from the given document
+    """
+    resource = urllib.request.urlopen(url)
+    content = resource.read().decode('utf-8')
+    data = content.splitlines()
+    dlist = print('\n'.join('{}: {}'.format(*k) for k in enumerate(data)))
+    return dlist
+    
+
+    
+
 
 # Main function
 def main():
+    fetch_url(url)
     return
 
 
